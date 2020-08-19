2bc219e4a22ea913336a3a5960086bcb1954afaf
nikki156 <nikitachavan214@gmail.com>
2020-08-19 13:58:38 +0530

    layout done

---
+++ main.js
@@ -0,0 +1,59 @@
+ 
+todos=[{
+    id:1,
+    title:'Meeting',
+    description:'Meeting at 6;00 pm with VC'
+},{
+    id:2,
+    title: 'College',
+    description:'vbfubvubfb'
+}];
+
+var show_todo=document.querySelector('.show_todo');
+
+
+if(todos.length==0){
+    show_todo.textContent = 'No todos to display';
+}
+else{
+    todos.forEach(function(todo){
+
+        var todo_wrapper = document.createElement('div');
+
+        var wrapper_div = document.createElement('div');
+        var action_wrapper = document.createElement('div');
+
+        var title = document.createElement('h3');
+        var description = document.createElement('h5');
+
+        title.textContent = todo.title;
+        description.textContent = todo.description;
+
+        var deleteButton = document.createElement('button');
+        var editButton = document.createElement('button');
+
+        deleteButton.addEventListener('click',function(){
+            console.log(todo.id);
+            //deleteTodo
+        })
+
+        editButton.addEventListener('click',function(){
+            console.log(todo.id);
+            //editTodo
+        })
+
+        deleteButton.textContent = 'Delete';
+        editButton.textContent = 'Edit';
+
+        wrapper_div.appendChild(title);
+        wrapper_div.appendChild(description);
+
+        action_wrapper.appendChild(deleteButton);
+        action_wrapper.appendChild(editButton); 
+
+        todo_wrapper.appendChild(wrapper_div);
+        todo_wrapper.appendChild(action_wrapper);
+
+        show_todo.appendChild(todo_wrapper);
+    })
+}
